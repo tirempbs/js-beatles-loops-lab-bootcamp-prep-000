54bfdf0153900be14662d426312e36ff60be34af
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-07 22:00:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var strings = [];
+  for (let i = 0; i < musicians.length; i++) {
+      strings.push(`${musicians[i]}` + " plays " + `${instruments[i]}`);
+  }
+  return strings;
+}
+
+function johnLennonFacts(facts) {
+  var loudFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    loudFacts.push(`${facts[i]}` + "!!!");
+    i++;
+  }
+  return loudFacts;
+}
+
+function iLoveTheBeatles(i) {
+ var love = [];
+ var n = 0;
+ function increment() {
+   n = n + 1;
+ }
+ do {
+   love.push("I love the Beatles!");
+   i++;
+ } while (i + increment() < 15);
+ return love;
+}
